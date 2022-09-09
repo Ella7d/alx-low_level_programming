57e0a55ab33cd9b0b30dbe615fdb020008dd8820
Ella7d <ella.debesai@gmail.com>
2022-09-09 16:16:31 -0700

    89

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alaways 0 (sucsess)
+ *
+ */
+int main(void)
+{
+	int a, b;
+	{
+	for (a = 0; a < 10; a++)
+	putchar(a);
+	}
+	{
+	for (b = 0; b < 10; b++)
+	putchar(b);
+	}
+	putchar(\n);
+}
