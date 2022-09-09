c032893b5c734a70487169a4dda892619e113b3c
Ella7d <ella.debesai@gmail.com>
2022-09-08 17:21:16 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
-#include <stdio.h>                                                              #include <string.h>
+#include <stdio.h>                                                              
+#include <string.h>
+
 /**
  *
  * main - Entry point
