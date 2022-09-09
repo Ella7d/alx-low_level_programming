c476f9e4eb656c4d1f08e3c807f1035c4b100aea
Ella7d <ella.debesai@gmail.com>
2022-09-08 17:43:48 -0700

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * msin - Entry point
+ *
+ * return: always 0
+ */
+
+int main(void)
+{
+	int intType;
+	long long longlongType;
+	long longType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of a char: %zu byte(s)\n", size of(charType));
+
+	printf("Size of an int: %d bytes(s)\n", sizeof(intType));
+
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(longType));
+
+	printf("size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
+}
