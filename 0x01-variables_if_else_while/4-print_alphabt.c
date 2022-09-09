464503569c6916b891f36b1e62e0c37ec14cc6e2
Ella7d <ella.debesai@gmail.com>
2022-09-09 14:26:29 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	char lc;
 
-	for (lc = a; lc = z; lc++)
+	for (lc = 'a'; lc = 'z'; lc++)
 	{
-	if (lc != e && lc != q)
+	if (lc != 'e' && lc != 'q')
 	putchar(lc);
 	putchar('\n');
 	}
