d4b7e409ee6390177aed8231dc346aedfd0c7114
Ella7d <ella.debesai@gmail.com>
2022-09-09 13:15:55 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * return : Always 0 (success)
+ *
+ */
+
+int main(void)
+	{
+	char lca;
+
+	for (lca = 'a'; lca <= 'z'; lca++)
+	putchar(lca);
+	putchar(\n);
+	return (0);
+	}
