f5b669065db0cfe252c968ef6a4d3c349a25c891
Ella7d <ella.debesai@gmail.com>
2022-09-09 10:30:24 -0700

    0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/** 
+ * Main - entry point
+ * Description -pos neg zero
+ * Return: int 0
+ **/
+
+int main(void)
+
+{
+     
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+         if (n>0)
+	      {
+	     	 printf("%d is positive/n",n)
+		 }	 
+       else
+         if (n==0)
+	      {
+	      printf("%d is zero/n",n)
+	      }	
+	 else(n<0)
+	      {
+	      ("%d is negative/n",n)
+	      }	 
+ 	 return (0);
+
+}
