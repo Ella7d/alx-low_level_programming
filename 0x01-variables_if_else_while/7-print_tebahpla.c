f5ccfbbd8f6f987cd2568345c5ae194dde61e678
Ella7d <ella.debesai@gmail.com>
2022-09-09 15:29:32 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alaways 0 (success)
+ *
+ */
+int main(void)
+{
+	char c = z;
+	{
+	for (c = 'z'; c >= 'a'; c--)
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
