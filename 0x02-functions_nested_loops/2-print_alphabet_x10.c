92be83805467682229fc40e6c20cab61899abc65
Ella7d <ella.debesai@gmail.com>
2022-09-13 11:18:34 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,16 @@
  *
  */
 void print_alphabet_x10(void)
+
 {
 	char lc;
 	int n;
+
 	for (n = 0; n <= 10; n++)
 
 	{
 	for (lc = 'a'; lc <= 'z'; lc++)
 	_putchar(lc);
-	}
-
 	_putchar('\n');
+	}
 }
