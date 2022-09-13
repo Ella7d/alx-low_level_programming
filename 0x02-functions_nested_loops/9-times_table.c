40bc6c867ba02e20d45c475d2d847e3f528afcc3
Ella7d <ella.debesai@gmail.com>
2022-09-13 16:18:53 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - multiples of 9 from 0
+ * r: row c:column d: digits of numbers
+ * Return: 0
+ *
+ */
+void times_table(void)
+{
+	int c, r, d;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				d = (r * c);
+				if ((d / 10) > 0)
+				{
+					_putchar((d / 10) > '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				_putchahr((d % 10) + '0');
+				if (c < 9)
+				{
+					_putchar(',');
+					_putchahr(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+
+
