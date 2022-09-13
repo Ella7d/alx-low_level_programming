de9270b405eb796457a3377dfa3e9b05bbec28c7
Ella7d <ella.debesai@gmail.com>
2022-09-13 13:03:02 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints signs
+ * @n: integer
+ * Return: 0
+ *
+ */
+int print_sign(int n)
+{
+	int i;
+
+	if (i > 0)
+	return (1);
+	{
+	else
+	if (i == 0)
+	return (0);
+	}
+	{
+	else
+	return (-1);
+	}
+}
+
