f97643169737c3f56a066928f95bd257695c987a
Ella7d <ella.debesai@gmail.com>
2022-09-13 12:01:46 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks alpha
+ * @c: int
+ * Return: 0
+ *
+ */
+int _isalpha(int c)
+{
+	 int i;
+	for (i = 'a'; i <= 'z'; i++)
+	for (i = 'A'; i <= 'Z'; i++)
+	if (c == i)
+	{
+	return (1);
+	}
+	return (0);
+}
+
