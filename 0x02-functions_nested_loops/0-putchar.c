66aae394628a82193d3e0e12d1bda57d0998bdac
Ella7d <ella.debesai@gmail.com>
2022-09-13 07:44:29 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+void main(void)
+{
+	{
+	char ch = '_putchar';
+	putchar(ch);
+	}
+	return (0);
+}
+
+
+
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+
+#ifndef MAIN_H_
+#DEFINE MAIN_H_
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int  main(void)
+{
+int _putchar(char c);
+return(0);
+}
+#ENDIF
