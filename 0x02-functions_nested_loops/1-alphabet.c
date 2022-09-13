6d0630dba0141cf235e2912687a11b12b84143a6
Ella7d <ella.debesai@gmail.com>
2022-09-13 09:42:47 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	return (0);
+}
