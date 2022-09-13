08651beda13eeaa2e804f52fa131ef587a6fa998
Ella7d <ella.debesai@gmail.com>
2022-09-13 12:27:10 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,10 +9,12 @@ int _isalpha(int c)
 {
 	 int i;
 	 
-	for ((i = 'a'; i <= 'z'; i++) || (i = 'A'; i <= 'Z'; i++))
-	if (c == i)
+	for ((i = 'a'; i <= 'z'; i++) || (j = 'A'; j <= 'Z'; j++))
+	{
+	if ((c == i) || (c == j))
 	{
 	return (1);
+	}
 	}
 	return (0);
 }
