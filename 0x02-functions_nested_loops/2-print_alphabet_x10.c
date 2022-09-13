21c3e667cd9828d003a596de95dc306c6a9f845c
Ella7d <ella.debesai@gmail.com>
2022-09-13 11:07:32 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char lc;
+	int n;
+	for (n = 0; n <= 10; n++)	
+	{
+	for (lc = 'a'; lc <= 'z'; lc++)
+	_putchar(lc);
+	}
+	_putchar('\n');
+}
