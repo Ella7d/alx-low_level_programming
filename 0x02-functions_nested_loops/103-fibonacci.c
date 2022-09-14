2ffd751b4e663331d355609c96256259cb5bbd4b
Ella7d <ella.debesai@gmail.com>
2022-09-14 11:13:57 -0700

    14

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+ */
+ int main(void)
+{
+	unsigned long f1 = 0, f2 =2 ,f3 , sum = 0;
+
+	for (num = 0; num <= 4000000; num++)
+	{
+		f1 = f2;
+		f2 = f3;
+		f3 = f1 + f2;
+	
+		if ((f1 % 2) == 0)
+		num += f1;
+	}
+	printf("%lu\n", num);
+	return (0);
+}
+
