8632df59c2fd89235e658efc2a74c87471a8933e
Ella7d <ella.debesai@gmail.com>
2022-09-13 17:25:03 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * add - add 3 numbers
+ * @a: intger
+ * @b: 2nd int sum: sum of a and b
+ * Return: 0
+ *
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+
+
+
+
