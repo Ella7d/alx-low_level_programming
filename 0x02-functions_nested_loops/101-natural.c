02e318b559d9b30a8b49675a723c488db6cb7330
Ella7d <ella.debesai@gmail.com>
2022-09-14 04:09:10 -0700

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 /**
  * main - entry pt
- * @i: integer j: intger sum: sum of numbers
+ *
  * Return: 0
  *
  */
-int_sum_multiples(int)
+int main(void)
 {
-	int i;
+	int i, sum;
 
 	sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 	if ((i % 3 == 0) || (i % 5 == 0))
-	sum + = i;
+	sum += i;
 	printf("%d\n", sum);
 	}
 	retutn(0);
