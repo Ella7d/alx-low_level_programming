688b58040de6bcec5258c833f26e3f9a6f590488
Ella7d <ella.debesai@gmail.com>
2022-09-14 11:19:27 -0700

    14

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,14 +7,13 @@
  */
  int main(void)
 {
-		unsigned long f1 = 0, f2 =2 ,f3 , num = 0;
+	unsigned long f1 = 0, f2 = 1, f3, num = 0;
 
 	for (num = 0; num <= 4000000; num++)
 	{
 		f1 = f2;
 		f2 = f3;
 		f3 = f1 + f2;
-	
 		if ((f1 % 2) == 0)
 		num += f1;
 	}
