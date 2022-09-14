711d6de20e6c652a68aa1a36efa87ec48e12614e
Ella7d <ella.debesai@gmail.com>
2022-09-14 10:09:54 -0700

    14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <main.h>
+/**
+ * main - entry point
+ * @i: number j: number
+ * Return: 0
+ *
+ */
+int main()
+{
+	int f1 = 1, f2 = 2, sum = 0, num = 50;
+
+	for (num = 0; num <= 50; num++)
+	{
+	if (f1 == 1)
+		return (1);
+	}
+	else if (f1 > 1)
+	{
+		sum = f1 + f2;
+		while (sum < num)
+		{
+			printf ("%d\n", sum);
+			f1 = f2;
+			f2 = sum;
+			sum = f1 + f2;
+		}
+	}
+}
