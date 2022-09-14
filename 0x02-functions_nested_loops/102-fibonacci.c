64bccad41b3b5b28d61cef0e91f685410dc6f958
Ella7d <ella.debesai@gmail.com>
2022-09-14 10:18:15 -0700

    14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,12 +9,12 @@ int main()
 {
 	int f1 = 1, f2 = 2, sum = 0, num = 50;
 
-	for (num = 0; num <= 50; num++)
 	{
+	for (num = 0; num <= 50; num++)
+	
 	if (f1 == 1)
 	{
 		return (1);
-	}
 	}
 	else if (f1 > 1)
 	{
@@ -26,5 +26,6 @@ int main()
 			f2 = sum;
 			sum = f1 + f2;
 		}
+	}
 	}
 }
