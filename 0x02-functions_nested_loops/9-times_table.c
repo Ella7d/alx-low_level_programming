97e9e2ac03199afaf1c5a7b02d8895bce4c15cdd
Ella7d <ella.debesai@gmail.com>
2022-09-13 17:00:33 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,28 +12,25 @@ void times_table(void)
 	for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		for (c = 1; c <= 9; c++)
+		{
+			d = r * c;
+			of (d < 10)
 			{
-				d = (r * c);
-				if (d > 10)
-				{
-					_putchar((d / 10) > '0');
-				}
-				else
-				{
-					_putchar(' ');
-				}
-				_putchar((d % 10) + '0');
-				if (c < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(d% 10 + '0');	
 			}
-			_putchar('\n');
-		}
+			else if (multi >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(d / 10 + '0');
+				_putchar(d % 10 + '0');
+			}
+		}						_putchar(10);
+	}
 }
 
 
