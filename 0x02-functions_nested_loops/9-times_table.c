e53adc6532e5971a78f0b16f65096ac731d2df7c
Ella7d <ella.debesai@gmail.com>
2022-09-13 17:12:15 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(d % 10 + '0');	
+				_putchar(d % 10 + '0');
 			}
 			else if (d >= 10)
 			{
@@ -29,7 +29,8 @@ void times_table(void)
 				_putchar(d / 10 + '0');
 				_putchar(d % 10 + '0');
 			}
-		}						_putchar(10);
+		}
+			_putchar(10);
 	}
 }
 
