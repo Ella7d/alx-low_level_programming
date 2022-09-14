85cfbb97fc932aefcfe1b39c87b76999d8d8d5d2
Ella7d <ella.debesai@gmail.com>
2022-09-14 10:47:09 -0700

    14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int f1 = 1, f2 = 2, f3, num;
+	unsigned long f1 = 1, f2 = 2, f3, num;
 
 	for (num = 0; num < 50; num++)
 	{
 	f3 = f1 + f2;
-	printf("%d", f3);
+	printf("%lu", f3);
 	f1 = f2;
 	f2 = f3;
 	if (num == 49)
