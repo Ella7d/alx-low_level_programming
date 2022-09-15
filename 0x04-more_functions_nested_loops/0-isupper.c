36a998490de7584d4d9afe5c5e17afb77695d869
Ella7d <ella.debesai@gmail.com>
2022-09-15 09:16:31 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * _isupper - if char isuppercase or not
+ * c: character input
+ * Return: 1 (success) 0 (failure)
+ *
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (_isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,13 @@
+Readme file
+1 upper case
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
