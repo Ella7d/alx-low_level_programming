46589f1d8af320655edd04499042e9429fd2d2f5
Ella7d <ella.debesai@gmail.com>
2022-09-15 10:46:40 -0700

    2

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+ void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar("%d", i);
+		_putchar("\n");
+		{
+			return (0);
+		}
+}
