ef528884b3cb303488fe39367e34e57dc82c6b7f
Ella7d <ella.debesai@gmail.com>
2022-09-15 13:05:21 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: integer number
+ */
+void print_line(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar(_ + '0');
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+		_putchar('\n');
+}
