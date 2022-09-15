af52454d8bf65758cc35ba00cb41829c435c4a55
Ella7d <ella.debesai@gmail.com>
2022-09-15 16:25:13 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: num of # to be printed
+ *
+ */
+void print_square(int size)
+{
+	int i = 0, j;
+
+	while (i < size && size > 0)
+	{
+	j = 0;
+	while (j < size)
+	{
+	_putchar('#');
+	j++;
+	}
+	_putchar('\n');
+	i++;
+	}
+	if (i == 0)
+	_putchar()'\n';
+}
