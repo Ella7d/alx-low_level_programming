c2941ce3e4d336d30b6cdc2e4090238fb6fd6f9f
Ella7d <ella.debesai@gmail.com>
2022-09-15 16:47:25 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,13 +8,17 @@
 
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
@@ -28,7 +32,6 @@ int main(void)
 		{
 			putchar(' ');
 		}
-		i++;
 	}
 	putchar('\n');
 	return (0);
