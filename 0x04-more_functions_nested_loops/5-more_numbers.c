0ee9461afbce84a40e4d4a63a3c8698525c42cdd
Ella7d <ella.debesai@gmail.com>
2022-09-15 17:38:44 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,26 +3,28 @@
 /**
  * more_numbers - prints numbers
  *
- *
+ * Return: void
  */
 
 void more_numbers(void)
 {
-	int a, b;
+	char a, b;
+	int i = 0;
 
-	for (a = 0; a <= 10; a++)
+		while (i < 10)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-
+			a = b;
 			if (b > 9)
 		{
-			_putchar(b / 10);
+			_putchar('1');
+			b = a % 10;
 		}
 
-		_putchar((b % 10) + '0');
+		_putchar('0' + c);
 		}
 	_putchar('\n');
-
+	i++;
 	}
 }
