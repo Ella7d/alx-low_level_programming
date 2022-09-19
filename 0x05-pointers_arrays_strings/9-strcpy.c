ea34ab85dca66f6423694a2ed38e5f1bb0ea8461
Ella7d <ella.debesai@gmail.com>
2022-09-19 14:01:03 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - copy string
+ * @dest:char string
+ * @src:char string
+ * Return: dest
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	dest[i] = src[i]
+	for (i = 0; src[i] == '\0'; i++)
+	return (dest);
+}
+
