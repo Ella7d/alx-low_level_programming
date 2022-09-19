448e4b5958184a6dce347ef29c91e8619edf10f4
Ella7d <ella.debesai@gmail.com>
2022-09-19 14:28:03 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,11 @@
  */
 void print_rev(char *s)
 {
-	int i;
-	int len;
-	char s;
-	for (i = len - 1; i >= 0; i--)
-	{
-	strrev(s[i]);	
-	}
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+	_putchar(s[i]);
+	_putchar('\n');
 }
