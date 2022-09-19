bbe6ad568b160db652dd8cc11337d49e771a7bf6
Ella7d <ella.debesai@gmail.com>
2022-09-19 16:00:41 -0700

    11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - generates keygen
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int n = 0, i = 0;
+	time_t t;
+
+	srand((unsignes int) time(&t));
+	while (c < 2772)
+	{
+		i = rand() % 128;
+		if ((i + n) > 2772)
+			break;
+		i = i + n;
+		printf("%i", n);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
+
