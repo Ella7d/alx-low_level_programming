336a3343b32c3b09728c5a8b71ecad31fd237125
Ella7d <ella.debesai@gmail.com>
2022-09-19 14:04:28 -0700

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = -1;
 
-	dest[i] = src[i]
-	for (i = 0; src[i] == '\0'; i++)
+	dest[i] = src[i];
+	for (i = 0; src[i] != '\0'; i++)
 	return (dest);
 }
 
