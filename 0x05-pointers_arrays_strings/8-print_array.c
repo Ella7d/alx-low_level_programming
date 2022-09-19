d41a75e80f826949fb379c9bc2f960adc560638b
Ella7d <ella.debesai@gmail.com>
2022-09-19 11:22:00 -0700

    7

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints elements of array
+ * @a: pointer
+ * @n: integer
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i, arr[];
+	int length = sizeof(arr[0]);
+
+	for (i = 0; i < length; i++)
+		_putchar(arr[i]);
+	_putchar('\n');
+}
+
