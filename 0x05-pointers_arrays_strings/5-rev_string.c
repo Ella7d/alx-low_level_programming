6c58848797a67855864a33cf901011502f2ce5c7
Ella7d <ella.debesai@gmail.com>
2022-09-19 14:44:47 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ * @s: char type string
+ * Return: 0
+ *
+ */
+void rev_string(char *s)
+{
+	int i, length, mid;
+	char temp;
+
+	for (length = ; s[len] != '\0'; length++)
+		i = 0;
+	mid = length / 2;
+	while (mid--)
+	{
+		temp = s[length - i - 1];
+		s[lenght - i - 1] = s[i];
+		s[i] temp;
+		i++;
+	}
+}
+
