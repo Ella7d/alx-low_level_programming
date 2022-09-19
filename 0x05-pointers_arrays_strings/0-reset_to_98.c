e9df758a12ffd4a44508237e5dfade6e5c26dc63
Ella7d <ella.debesai@gmail.com>
2022-09-19 01:30:03 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update pointer value to 98
+ * @i: integer
+ * Return: int
+ *
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int *p;
+
+	n = 98;
+	*p = &n;
+}	
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,12 @@
+readme file of pointers, arrays and strings
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
