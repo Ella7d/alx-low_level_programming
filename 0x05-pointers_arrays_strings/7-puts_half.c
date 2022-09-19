2f26deb894eaf9fb7266b5c765b968718f4344b3
Ella7d <ella.debesai@gmail.com>
2022-09-19 10:26:16 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,16 +8,17 @@
 void puts_half(char *str)
 {
 	int i;
-	int n = 0;
+	long int n = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
-	if (n / 2 == 0)
+	if (n % 2 == 0)
 	{
 	_putchar(str[i] / 2);
 	}
-	else if (n / 2 != 0)
+	else if (n % 2 != 0)
 	{
 	_putchar((str[i] - 1) / 2);
 	}
+	_putchar('\n');
 }
 
