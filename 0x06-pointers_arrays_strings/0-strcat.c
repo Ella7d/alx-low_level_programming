5157a06f7d1f7cfb54d8db3d4cd84b3636e52770
Ella7d <ella.debesai@gmail.com>
2022-09-21 10:16:47 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<string.h>
 /**
  * *_strcat - concatinate strings
  * @dest: string
@@ -12,11 +13,11 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-		for (j = 0; src[j] != '\0'; j++)
-		{
-		dest[i] = src[j];
-		i++;
-		}
-		dest[i] = '\0';
+	for (j = 0; src[j] != '\0'; j++)
+	{
+	dest[i] = src[j];
+	i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
