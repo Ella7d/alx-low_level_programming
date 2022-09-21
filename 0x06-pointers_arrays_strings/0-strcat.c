8e208bded2ae4e89d24b88d83f528a4c8b9094f9
Ella7d <ella.debesai@gmail.com>
2022-09-21 09:05:09 -0700

    2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,14 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	char dest, src;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		for (j = 0; src[j] != '\0'; j++)
+		{
 		dest[i] = src[j];
-		j++;
 		dest[i] = '\0';
+		}
 	}
 	return (dest);
 }
