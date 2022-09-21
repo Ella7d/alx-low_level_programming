ce53e36b2e30d99e84ebdd148cf9053449d9b666
Ella7d <ella.debesai@gmail.com>
2022-09-21 15:07:46 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,14 +8,15 @@
 char *cap_string(char *c)
 {
 	int i, j;
-	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+       char sep[] = " \t\n,;.!?\"(){}";
+
 	for (i = 0;c[i] != '\0'; i++)
 	{
 		if (c[0] >= 97 && c[0] <= 122)
 			c[0] = s[0] -32;
-		for (j = 0; delimeters[j] != '\0'; j++)
-			if (c[i] == sep[j] && c[i + 1] >= 97 && c[i + 1] <= 122)
-				c[i + 1] = c[i + 1] - 32;
+				for (j = 0; delimeters[j] != '\0'; j++)
+					if (c[i] == sep[j] && c[i + 1] >= 97 && c[i + 1] <= 122)
+						c[i + 1] = c[i + 1] - 32;
 	}
 	return (c);
 }
