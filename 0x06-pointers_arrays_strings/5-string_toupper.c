63653d59b48fbb748d1d123cd4781fb8df04223f
Ella7d <ella.debesai@gmail.com>
2022-09-21 14:45:11 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * *string_toupper
- *
+ * @lc: string
  * Return: 0
  */
-char *string_toupper(char *)
+char *string_toupper(char *lc)
 {
-	char *lc;
-	int n, i;
+	int i;
 
 	for (i = 'a'; lc[i] != '\0'; i++)
 	{
 		if (lc[i] >= 'a' && lc[i] <= 'z')
 			lc[i] = lc[i] - 32;
 	}
+	return (lc);
 }
 
 
