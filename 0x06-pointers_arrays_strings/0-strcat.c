f2caed79d3db33d0ea4e0fdf316bd23f8076c751
Ella7d <ella.debesai@gmail.com>
2022-09-20 23:23:44 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strcat - concatinate strings
+ * @dest: char
+ * @src: char
+ * Return: always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	char s1;
+	char s2;
+
+	strcat (s1, s2);
+	return (0);
+}
