1234f4a0c2b6b0f93b7857e6a7c14a85841fe1b0
Ella7d <ella.debesai@gmail.com>
2022-09-21 11:12:10 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_strncpy - copies one string into another upto n
+ * _strncpy - copies one string into another upto n
  * @dest: string input
  * @src: string input
  * @n: string count
@@ -12,9 +12,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
-		for (j = 0; src[j] != '\0')
-		if (dest[j] < n)
+		for (j = 0; src[j] != '\0'; j++)
+		{
+			if (dest[j] < n)
 			src[j] = dest[i];
+		}
 	}
 	src[j++] = '\0';
 }
