1994505df6b46c5777cf232808e7e77ca4d64944
Ella7d <ella.debesai@gmail.com>
2022-09-21 14:51:26 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *string_toupper
+ * string_toupper - change to lower case
  * @lc: string
  * Return: 0
  */
@@ -8,7 +8,7 @@ char *string_toupper(char *lc)
 {
 	int i;
 
-	for (i = 'a'; lc[i] != '\0'; i++)
+	for (i = 0; lc[i] != '\0'; i++)
 	{
 		if (lc[i] >= 'a' && lc[i] <= 'z')
 			lc[i] = lc[i] - 32;
