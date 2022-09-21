3b4a820013a07ac83e081612b2ec7184fdfee999
Ella7d <ella.debesai@gmail.com>
2022-09-20 23:50:11 -0700

    1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,6 @@ char *_strcat(char *dest, char *src)
 	char s1;
 	char s2;
 
-	strcat (s1, s2);
+	_strcat (s1, s2);
 	return (0);
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *_strncat - concantinate 2 strings upto n
+ * @dest: string 
+ * @src: string
+ * Return: 0 
+ * 
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char s1;
+	char s2;
+
+	_strncat (s1, s2, n);
+	return (0);
+}
