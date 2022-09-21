dfe442cefd9516f8451b2576f0c4d7fdaadecc8d
Ella7d <ella.debesai@gmail.com>
2022-09-21 14:25:57 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,17 +7,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	char s1, s2;
-	int i, j;
+	int i, c;
 
-	for (i = 0; s1[i] == s2[i] && s1[i] != '\0'; i++)
-		if (s1[i] == s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else
-		{
-			return (0);
-		}
+	while (s1[i] == s2[i] && (s1[i] != '\0'|| s2[i] != '\0'))
+	{
+		i++;
+	}
+		c = s1[i] - s2[i];
+			return (c);
 }
 
