7adacc8b3b92e08c2f2bdf4ea816b33c10c2c281
Ella7d <ella.debesai@gmail.com>
2022-09-21 11:29:12 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,10 +10,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 		{
-			if (dest[i] < n)
-			src[i] = dest[i];
+			if (dest[i] != '\0')
+			dest[i] = src[i];
 		}
 	dest[i] = '\0';
 	return (dest);
