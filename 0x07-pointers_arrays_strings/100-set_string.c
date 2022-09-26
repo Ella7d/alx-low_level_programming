a83c952955b422686dcfcb33959fa62942e9dddc
Ella7d <ella.debesai@gmail.com>
2022-09-26 15:36:17 -0700

    f

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets a string
+ * @s: pointer to a a pointer
+ * @to: character
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
