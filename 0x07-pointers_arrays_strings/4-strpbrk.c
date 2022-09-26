37a8af5c3802aa92bb31493cbbe74f89ef2cb2c3
Ella7d <ella.debesai@gmail.com>
2022-09-26 12:19:23 -0700

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - search a string for any of a set of bytes
+ * @s:string to search
+ * @accept: bytes tosearch for
+ * Return: s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (*s == accept[i])
+			return (s);
+	}
+	s++;
+	{
+return (0);
+	}
+}
+
