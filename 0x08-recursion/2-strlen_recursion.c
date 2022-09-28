4e613e000548b7aa2729ac086cfa020234f221e8
Ella7d <ella.debesai@gmail.com>
2022-09-28 09:12:30 -0700

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * _strlen_recursion - gives length of string
  * @s: string to be measured
  * Return: s
@@ -10,5 +9,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return (1 + _strlen_recursion(s));
+		return (1 + _strlen_recursion(s + 1));
 }
