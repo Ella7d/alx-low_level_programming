61bad16fdc10f8b4db0aef6a7e5a595fdadc4004
Ella7d <ella.debesai@gmail.com>
2022-09-28 07:53:30 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,11 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 	_putchar(*s);
-	return (s);
 	s++;
+	_puts_recursion(s);
 	}
 	_putchar("\n");
-		return (0);
-		_puts_recursion(s);
+		return;
 }
 
 
