c29a0014bf30b8bd8223215227d4f9291d2707f6
Ella7d <ella.debesai@gmail.com>
2022-09-28 06:41:01 -0700

    6

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  _puts_recursion - prints a string
+ *  @s: string to be printed
+ *  Return: s
+ */
+void _puts_recursion(char *s)
+{
+	s != '\0';
+	{
+	_putchar("%s", s);
+	s++;
+	return (s);
+	}
+return (0);
+}
+
+
