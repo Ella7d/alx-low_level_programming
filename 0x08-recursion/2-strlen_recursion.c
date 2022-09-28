16dcadbfbc999f11b04b9e2664e812d3ccc957ff
Ella7d <ella.debesai@gmail.com>
2022-09-28 09:04:25 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ * _strlen_recursion - gives length of string
+ * @s: string to be measured
+ * Return: s
+ */
+int _strlen_recursion(char *s)
+{
+	if (s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	
+		else
+		{
+			return ('\n');
+		}
+	}
+}
