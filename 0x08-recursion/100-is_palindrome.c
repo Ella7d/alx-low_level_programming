92a2dc7329c847132f9ebd210d3a7dcdb6782a6b
Ella7d <ella.debesai@gmail.com>
2022-09-28 13:42:33 -0700

    7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int palindromeChecker(char *str, int len, int i)
 	if (i < len && str[i] == str[len])
 
 		return (palindromeChecker(str, len - 1, i + 1));
-		
+
 	if (str[i] != str[len])
 
 		return (0);
@@ -30,7 +30,7 @@ int _strlen_recursion(char *s)
 {
 
 		if (*s != '\0')
-		
+
 			return (1 + _strlen_recursion(s + 1));
 
 		return (0);
