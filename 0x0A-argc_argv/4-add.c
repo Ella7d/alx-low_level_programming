af9b10ab5038ee5c014f58a39c8d901f462414b7
Ella7d <ella.debesai@gmail.com>
2022-09-30 15:02:19 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - adds two number
+ * @argv: vector
+ * @argc: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' ||)argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
