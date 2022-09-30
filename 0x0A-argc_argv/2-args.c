f9ed39c5d49beaee9c2d735e2c44cc4e953aa5c1
Ella7d <ella.debesai@gmail.com>
2022-09-30 14:46:00 -0700

    1

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 /**
 * main - print product of argument numbers
@@ -9,12 +8,11 @@
 */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	int i;
 
+	for  (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
