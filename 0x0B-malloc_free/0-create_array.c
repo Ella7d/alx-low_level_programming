83df33479552d2e32571e51ba1e8c861792282a3
Ella7d <ella.debesai@gmail.com>
2022-10-03 12:09:25 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,14 +11,14 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *a;
 	
-	if (i <= size && i != 0)
+	if (i < size && i != 0)
 	{
 	a = malloc(sizeof (char) * size);
 	a[i] = c;
 	i++;
 	return (a);
 	}
-	else if (i == 0 && a == '\0')	
+	else if (size == 0 && a == '\0')	
 	{
 		return ("\0");
 	}
