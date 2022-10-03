9580abbb6dd2b94dc5106024c5fe0fd6240fd548
Ella7d <ella.debesai@gmail.com>
2022-10-03 13:29:18 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - copies string to allocated memory
+ * @str: string to be coppied
+ * Return: ptr of duplicate on succes , null if fail
+ *
+ */
+char *_strdup(char *str)
+{
+	char s;
+	unsigned int i, j;
+
+	if (str == 0)
+	{
+		return ('\0');
+	}
+	for (i = 0; s[i] >= 0; i++);
+		s = malloc(sizeof(char) * (i + 1));
+	if (s == '\0')
+	{
+		return ('\0');
+	}
+	for (j = 0; j < i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
+}
+
+		
+
+
+
