e1a4e10af072e1bdb5d43056a2624723ac25239c
Ella7d <ella.debesai@gmail.com>
2022-10-04 14:10:28 -0700

    5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,13 +26,13 @@ char *argstostr(int ac, char **av)
 		for (i = 0; i < ac; i++)
 		{
 			for (j = 0; av[i][j]; j++)
-				{
-					s[k] = av[i][j];
-					k++;
-				}
-				s[k] = '\n';
+			{
+				s[k] = av[i][j];
 				k++;
 			}
-				s[k] = '\0';
-				return (s);
+			s[k] = '\n';
+			k++;
 			}
+		s[k] = '\0';
+		return (s);
+}
