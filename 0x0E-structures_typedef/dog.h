617952428fe5b785b95ec98d2c395ead36279a4c
Ella7d <ella.debesai@gmail.com>
2022-10-10 10:31:44 -0700

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,10 @@ struct dog
  * dog_i - typedefdog
  *
  */
-typedef struct dog d;
-	{
-		d_i.name = name;
-		d_i.age = age;
-		d_i.owner =owner;
-	}
+typedef struct dog dog_t;
+	int _putchar(char c);
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
 #endif
