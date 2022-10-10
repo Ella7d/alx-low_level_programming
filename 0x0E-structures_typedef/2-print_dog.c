04875f68945e8edc3aeea0625b0104ea0d93863b
Ella7d <ella.debesai@gmail.com>
2022-10-10 12:29:14 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,23 +2,26 @@
 /**
  * print_dog - prints dog info
  * @d: dog info
+ * Return: NULL if fail
+ *
  */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	return (0);
 	{
-		return (0);
-	}
-	if (d->name != NULL)
+	if (d->name)
 		printf("Name: %s\n", d->name);
 	else
 		printf("Name: (nil\n)");
 		
 	printf("Age: %.6f\n", d->age);
 	
-	if (d->owner != NULL)
+	if (d->owner)
 		printf("Owner: %s\n", d->owner);
 	else
 		printf("Owner: (nill)\n");
+	}
 }
+
 
