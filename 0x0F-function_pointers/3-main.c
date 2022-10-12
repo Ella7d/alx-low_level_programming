c256d878a12e8d9733cb2997168691e246166d12
Ella7d <ella.debesai@gmail.com>
2022-10-12 09:00:06 -0700

    5

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,15 +10,31 @@
 int main(int argc, char *argv[])
 
 {
-	register int a, b;
+	int a, b;
 	int (*fptr)(int, int);
 
 		if (argc != 4)
-			printf("Error\n"), exit(98);
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		if (argv[2][1])
+		{
+			printf("Error\n");
+			exit(99);
+		}
+
 		fptr = get_op_func(argv[2]);
-		if (!fptr)
-			printf("Error\n"), exit(99);
+		
+		if (fptr == NULL)
+		{
+			printf("Error\n");
+	       		exit(99);
+		}
+
 		a = atoi(argv[1]);
-		b = atoi(argv[3]);printf("%i\n", fptr(a, b));
+		b = atoi(argv[3]);
+		
+		printf("%d\n", fptr(a, b));
 		return (0);
 }
