5f6e7b5e25ca833e47c77b7b873c8cfa99ac0e58
Ella7d <ella.debesai@gmail.com>
2022-10-12 09:12:05 -0700

    7

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,10 +11,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size > 0 && cmp && size && array)
-		for (i = 0; i <= size; i++)
-			if (cmp(array[i]))
-				return (i);
-				return (-1);
+	if (cmp && array)
+	{
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+
+			return (i);
+	}
+	return (-1);
 }
 
