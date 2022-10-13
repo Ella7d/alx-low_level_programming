d491166a4220b4aaa350348ca9d7be9decef0686
Ella7d <ella.debesai@gmail.com>
2022-10-13 08:11:08 -0700

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,12 +14,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list vl;
 
 	va_start(vl, n);
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(vl, int));
 	if (separator && i < n - 1)
 		printf("%s", separator);
 	}
+
 	printf("\n");
 	va_end(vl);
-
+}
