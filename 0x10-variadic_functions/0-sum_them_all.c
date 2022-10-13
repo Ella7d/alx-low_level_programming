8b66d50385fdc77c3ca11c148e60adb303ffeb8d
Ella7d <ella.debesai@gmail.com>
2022-10-13 07:22:36 -0700

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,9 +13,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start (ap, n);
 	sum = 0;
+	if (n > 0)
+	{
 	for (i = 0; i <= n; i++)
 		sum += va_arg (ap, int);
 
 	va_end (ap);
 	return (sum);
+	}
+	return (0);
 }
