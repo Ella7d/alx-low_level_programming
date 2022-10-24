183b82ac2993e0cbc2e5af69d93ebd0fe06e1e88
ella7d <ella.debesai@gmail.com>
2022-10-24 13:44:48 -0700

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,16 +7,18 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *current;
-
+	listint_t *temp;
+	listint_t *current;
 	if (head != NULL)
 	{
 		current = *head;
 
-	while ((current = temp) != NULL)
-	{
+	while ((temp = current) != NULL)
+		{	
 		current = current->next;
 		free(temp);
-	}
+	
+		}
 	*head = NULL;
+	}
 }
