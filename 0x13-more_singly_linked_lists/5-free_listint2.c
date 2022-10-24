da3abb1ab5598d6693cff8d039cda5870e3a0f70
ella7d <ella.debesai@gmail.com>
2022-10-24 13:35:00 -0700

    4

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listsint2 - frees lists
+ * @head: headof list
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listsint_t *temp, *current;
+
+	if (head != NULL)
+	{
+		current = *head;
+
+	while ((current = temp) != NULL)
+	{
+		current = current->next;
+		free(temp);
+	}
+	*head = NULL;
+}
