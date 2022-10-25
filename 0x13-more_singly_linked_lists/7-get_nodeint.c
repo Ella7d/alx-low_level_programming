694a37181cd434f240c02791f8d1e4c10ec19008
ella7d <ella.debesai@gmail.com>
2022-10-25 12:09:47 -0700

    13

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,11 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (; i < index; i++)
 	{
-	if (head == NULL)
-		return (NULL);
+		if (head == NULL)
+			return (NULL);
 
-	head = head->next;
+		head = head->next;
 	}
 	return (head);
-
 }
