db4d548a227f6f3588312cb0ceb16559cbdbcf93
ella7d <ella.debesai@gmail.com>
2022-12-08 04:23:05 -0800

    1

--- 0x17-doubly_linked_lists/1-distinct_len.c
+++ 0x17-doubly_linked_lists/1-distinct_len.c
@@ -3,18 +3,16 @@
 /**
  * dlistint_len - Counts the number of elements in a linked dlistint_t list.
  * @h: The head of the dlistint_t list.
- *
  * Return: The number of elements in the dlistint_t list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t nodes = 0;
+size_t num_ele = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
 		h = h->next;
+		num_ele++;
 	}
-
-	return (nodes);
+	return (num_ele);
 }
