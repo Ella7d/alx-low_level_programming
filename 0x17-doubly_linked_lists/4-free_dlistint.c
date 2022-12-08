151f193b44f9fe7769d3afe32fe7965a9c703470
ella7d <ella.debesai@gmail.com>
2022-12-08 05:37:24 -0800

    4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,9 +9,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 
-	while (head)
+	while (head != NULL)
 	{
-		tmp = head->next;
-		free(head);
+		tmp = head->;
+		head = head->next;
+		free(tmp);
 	}
 }
